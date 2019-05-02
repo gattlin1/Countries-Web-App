123893f75880561c310377b260a25428aa601834
gattlin1 <gattlin1@live.missouristate.edu>
2019-05-01 21:05:45 -0500

    Started working on the twitter login

--- final_project/main.js
+++ final_project/main.js
@@ -1,21 +1,67 @@
 'use strict';
 
 const express = require('express');
-const app = express();
 const request = require('request');
 const url = require('./modules/createUrl');
 const quiz = require('./modules/quiz');
 const twitter = require('./modules/twitter');
 const passport = require('passport');
-const passportInfo = require('./login')
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const TwitterStrategy = require('passport-twitter').Strategy;
+
+const app = express();
+
+app.use(
+	expressSession({
+		secret : 'mySecretCode',
+		resave: false,
+		saveUninitialized: false
+	})
+);
 
 app.use(express.static('resources'));
-app.use(express.json()); 
-app.use(
-	express.urlencoded({
-		extended: true
-	})
-); 
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+const User = {
+
+};
+
+passport.use(new TwitterStrategy ({
+	consumerKey: 'iwZfS59lqo34WIQTy5mJjj9mf',
+	consumerSecret:'xt0BVxJ6kqVHBXPtDg7XqAwfTRucCwikY8saj6X4GeVfZd2g2d',
+	callbackURL: 'http://127.0.0.1:3000'
+},
+function(token, tokenSecret, profile, cb) {
+	User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+		return cb(err, user);
+	});
+}
+));
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', passport.authenticate('twitter',
+	{ failureRedirect: '/' }), function(req, res) {
+	console.log('i made it to the redirect');
+	res.redirect('/homepage');
+});
+
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(id, done) {
+	done(null, {
+		user: id
+	});
+});
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -100,25 +146,6 @@ app.post('/quiz', function(req, res) {
 		{status: twitterMsg});
 	res.json(numCorrect);
 });
-
-// app.get('/auth/twitter',
-// 	passport.authenticate('twitter'));
-
-// app.get('/auth/twitter/callback',
-// 	passport.authenticate('twitter', {failureRedirect: '/login'}),
-// 	function(req, res){
-// 		res.redirect('/')
-// 	});
-	
-	app.post(
-		'/login',
-		passport.authenticate('twitter', {
-			failureRedirect: '/',
-			successRedirect: '/homepage'
-		})
-	);
-	
-	
 
 const server = app.listen(3000, function() {
 	console.log(`Server is running on port ${server.address().port}`);
--- final_project/modules/twitter.js
+++ final_project/modules/twitter.js
@@ -1,13 +1,15 @@
 'use strict';
-let twit = require('twitter')
+let twit = require('twitter');
 let myTwitter = new twit({
 	consumer_key : 'iwZfS59lqo34WIQTy5mJjj9mf',
 	consumer_secret: 'xt0BVxJ6kqVHBXPtDg7XqAwfTRucCwikY8saj6X4GeVfZd2g2d',
 	access_token_key: '1120498407464947712-vKIB7LawSQJHADUrliGLvtVWRjBY6k',
 	access_token_secret: 'e9NJasVc0s4eBGJEqUwQaW6xgwWsFktM7xY6J1sOpWA6y',
 	timeout_ms: 60 * 1000,
-	strictSSL: true, 
-	})
+	strictSSL: true,
+});
+
+module.exports = myTwitter;
 
 
 
