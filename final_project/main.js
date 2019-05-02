7fba206bc1ebd6f6a0bafe4f2edc850ed415d1ad
gattlin1 <gattlin1@live.missouristate.edu>
2019-05-02 14:26:07 -0500

    Finished passport login for twitter. Added a logout feature. Added another template pug file that only the mainpages utilize to give the logout feature

--- final_project/main.js
+++ final_project/main.js
@@ -7,18 +7,17 @@ const quiz = require('./modules/quiz');
 const twitter = require('./modules/twitter');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
-const expressSession = require('express-session');
+const session = require('express-session');
 const TwitterStrategy = require('passport-twitter').Strategy;
 
 const app = express();
 
-app.use(
-	expressSession({
-		secret : 'mySecretCode',
-		resave: false,
-		saveUninitialized: false
-	})
-);
+
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false
+}));
 
 app.use(express.static('resources'));
 app.use(express.json());
@@ -27,37 +26,22 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-const User = {
-
-};
 
 passport.use(new TwitterStrategy ({
 	consumerKey: 'iwZfS59lqo34WIQTy5mJjj9mf',
-	consumerSecret:'xt0BVxJ6kqVHBXPtDg7XqAwfTRucCwikY8saj6X4GeVfZd2g2d',
-	callbackURL: 'http://127.0.0.1:3000'
+	consumerSecret: 'xt0BVxJ6kqVHBXPtDg7XqAwfTRucCwikY8saj6X4GeVfZd2g2d',
+	callbackURL: 'http://127.0.0.1:3000/auth/callback'
 },
 function(token, tokenSecret, profile, cb) {
-	User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-		return cb(err, user);
-	});
-}
-));
+	return cb(null, profile);
+}));
 
-app.get('/auth/twitter', passport.authenticate('twitter'));
-app.post('/', passport.authenticate('twitter',
-	{ failureRedirect: '/' }), function(req, res) {
-	res.redirect('/homepage');
+passport.serializeUser(function(user, cb) {
+	cb(null, user);
 });
 
-
-passport.serializeUser(function(user, done) {
-	done(null, user);
-});
-
-passport.deserializeUser(function(id, done) {
-	done(null, {
-		user: id
-	});
+passport.deserializeUser(function(obj, cb) {
+	cb(null, obj);
 });
 
 app.set('view engine', 'pug');
@@ -65,19 +49,28 @@ app.set('views', 'views');
 
 const ensureAuthenticated = function(req, res, next) {
 	if (req.isAuthenticated() === false) {
-		res.sendStatus(403);
+		res.redirect('/login');
 		return;
 	}
 
 	next();
 };
 
-app.get('/', function(req, res){
-	res.render('login');
+app.get('/login/twitter',
+	passport.authenticate('twitter'));
+
+app.get('/auth/callback',
+	passport.authenticate('twitter', { failureRedirect: '/login' }),
+	function(req, res) {
+		res.redirect('/');
+	});
+
+app.get('/', ensureAuthenticated, function(req, res) {
+	res.render('homepage');
 });
 
-app.get('/homepage', ensureAuthenticated, function(req, res) {
-	res.render('homepage');
+app.get('/login', function(req, res){
+	res.render('login');
 });
 
 app.get('/funFacts', ensureAuthenticated, function(req, res) {
@@ -118,9 +111,12 @@ app.get('/quiz', ensureAuthenticated, function(req, res) {
 		json: true
 	},
 	function(err, response, body) {
+		if (err || response.statusCode >= 400) {
+			res.sendStatus(500);
+			return;
+		}
 		quiz.getCountries(body);
 
-		// TODO: add error checks, possibly combine error statements into a callable function
 		const questions = {info: quiz.create()};
 
 		questions.info.forEach(function(question) {
@@ -142,7 +138,7 @@ app.post('/quiz', ensureAuthenticated, function(req, res) {
 		}
 	}
 
-	if (numCorrect > 7) {
+	if (numCorrect >= 7) {
 		twitterMsg = `My country flag knowledge is superb. I scored ${numCorrect} out of 10`;
 	}
 	else {
@@ -151,6 +147,11 @@ app.post('/quiz', ensureAuthenticated, function(req, res) {
 	twitter.post('statuses/update',
 		{status: twitterMsg});
 	res.json(numCorrect);
+});
+
+app.get('/logout', ensureAuthenticated, function(req, res) {
+	req.logout();
+	res.redirect('/');
 });
 
 const server = app.listen(3000, function() {
